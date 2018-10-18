0f7e805a2ff7529a8e17b6973f92f09d866defba
CRX <2777067079@qq.com>
2018-10-18 11:14:24 +0800

    Vue-cli

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <p class="app">{{msg}}</p>
+    <!-- <img src="./assets/logo.png">
+    <router-view/> -->
+    <button @click='btnHandle'>弹出框</button>
+  </div>
+</template>
+
+<script>
+// export default {
+//   name: 'App'
+// }
+import {toast} from 'mint-ui'
+export default {
+  name: 'hellworld',
+  data () {
+    return {
+      msg: '====wi==='
+    }
+  },
+  methods: {
+    btnHandle() {``
+      toast('提示信息')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
